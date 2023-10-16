98c80c05036f7e211473b8cf72bcae8f5a1111d5
MishaVerkhovskiy <mishaverxovskiy01@gmail.com>
2023-10-16 15:04:52 +0300

    Laba1

--- app/src/main/java/com/example/androidapplication/Name.kt
+++
@@ -1,5 +0,0 @@
-package com.example.androidapplication
-
-import java.io.Serializable
-
-data class Name(val name: String) : Serializable
--- app/src/main/java/com/example/androidapplication/OsnClass.kt
+++ app/src/main/java/com/example/androidapplication/OsnClass.kt
@@ -1,9 +1,6 @@
 package com.example.androidapplication
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -19,8 +16,6 @@ class OsnClass : AppCompatActivity() {
         var text = findViewById<TextView>(R.id.textView2)
         val button1 = findViewById<Button>(R.id.button1)
         val button2 = findViewById<Button>(R.id.button2)
-        val nextButton1 = findViewById<Button>(R.id.nextButton2)
-        val backButton1 = findViewById<Button>(R.id.backButton2)
 
         button1.setOnClickListener {
             button1.text = "Кнопка 1 нажата!"
@@ -34,17 +29,7 @@ class OsnClass : AppCompatActivity() {
             text.setText("Кнопка 2 нажата $counter2 раз")
         }
 
-        nextButton1.setOnClickListener {
-            val intent = Intent(this@OsnClass, Activity4::class.java);
-            startActivity(intent);
 
-        }
-
-        backButton1.setOnClickListener {
-            val intent = Intent(this@OsnClass, Activity3::class.java);
-            startActivity(intent);
-
-        }
     }
 
 }
