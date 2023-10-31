7fa64225e54cb1f4da7ea748c0ebb780dd7b18ab
MishaVerkhovskiy <mishaverxovskiy01@gmail.com>
2023-10-31 10:11:02 +0300

    Laba6

--- app/src/main/java/com/example/androidapplication/Authorization.kt
+++ app/src/main/java/com/example/androidapplication/Authorization.kt
@@ -3,6 +3,7 @@ package com.example.androidapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.provider.ContactsContract.Data
 import android.widget.Button
 import android.widget.EditText
@@ -26,27 +27,33 @@ class Authorization : AppCompatActivity() {
         }
 
         button.setOnClickListener {
-            val login = userLogin.text.toString().trim()
-            val pass = userPass.text.toString().trim()
+            Thread {
+                    val login = userLogin.text.toString().trim()
+                    val pass = userPass.text.toString().trim()
 
-            if(login == "" || pass == "") {
-                Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
-            }
-                val db = DatabaseHandler(this, null)
-                val isAuth = db.getUser(login, pass)
+                    if(login == "" || pass == "") {
+                        button.post{
+                            Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                    val db = DatabaseHandler(this, null)
+                    val isAuth = db.getUser(login, pass)
 
-                if(isAuth) {
-                    Toast.makeText(this, "Пользователь $login авторизован", Toast.LENGTH_LONG).show()
-                    userLogin.text.clear()
-                    userPass.text.clear()
+                    if(isAuth) {
+                        button.post{
+                            Toast.makeText(this, "Пользователь $login авторизован", Toast.LENGTH_LONG).show()
+                            userLogin.text.clear()
+                            userPass.text.clear()
 
-                    val intent2 = Intent(this@Authorization, Activity3::class.java)
-                    startActivity(intent2)
+                            val intent2 = Intent(this@Authorization, Activity3::class.java)
+                            startActivity(intent2)
+                        }
 
-                } else
-                    Toast.makeText(this, "Пользователь $login не авторизован", Toast.LENGTH_LONG).show()
-
+                    } else
+                        button.post{
+                            Toast.makeText(this, "Пользователь $login не авторизован", Toast.LENGTH_LONG).show()
+                        }
+            }.start()
         }
-
     }
 }
--- app/src/main/java/com/example/androidapplication/HelloActivity1.kt
+++ app/src/main/java/com/example/androidapplication/HelloActivity1.kt
@@ -2,6 +2,7 @@ package com.example.androidapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -20,7 +21,7 @@ class HelloActivity1 : AppCompatActivity() {
         private val TAG: String = HelloActivity1::class.java.simpleName
     }
 
-    val db = DatabaseHandler(this, null)
+    private lateinit var db: DatabaseHandler
 
     private lateinit var rvUsers: RecyclerView
     private val userAdapter = UserAdapter()
@@ -29,6 +30,8 @@ class HelloActivity1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_helloact1)
 
+        db = DatabaseHandler(this, null)
+
         val userLogin: EditText = findViewById(R.id.user_login)
         val userId: EditText = findViewById(R.id.user_id)
         val userPass: EditText = findViewById(R.id.user_pass)
@@ -36,32 +39,44 @@ class HelloActivity1 : AppCompatActivity() {
         val linkToAuth: TextView = findViewById(R.id.link_to_auth)
 
         val btnLoad: Button = findViewById(R.id.btnLoad)
+
         //ссылка на авторизацию
         linkToAuth.setOnClickListener {
             val intent = Intent(this, Authorization::class.java)
             startActivity(intent)
         }
+
         //регистрация нового пользователя
         button.setOnClickListener {
             val login = userLogin.text.toString().trim()
             val id = userId.text.toString().trim()
             val pass = userPass.text.toString().trim()
 
-            if(login == "" || id.isEmpty() || pass == ""){
+            if (login.isEmpty() || id.isEmpty() || pass.isEmpty()) {
                 Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
-            }
-            else {
+            } else {
                 val user = User(id.toInt(), login, pass)
-                db.addUser(user)
-                Toast.makeText(this, "Пользователь $login добавлен", Toast.LENGTH_LONG)
+                Thread {
+                    db.addUser(user)
+
+                    button.post {
+                        Toast.makeText(this, "Пользователь $login добавлен", Toast.LENGTH_LONG).show()
+                    }
+                }.start()
             }
         }
+
         //загрузка инфы из бд
         btnLoad.setOnClickListener {
-            val users = db.getAllUsers()
-            val usersLog = users.joinToString(separator = ",\n")
-            Log.d(TAG, "Users:\n $usersLog")
-            userAdapter.setData(users)
+            Thread {
+                val users = db.getAllUsers()
+                val usersLog = users.joinToString(separator = ",\n")
+                Log.d(TAG, "Users:\n $usersLog")
+
+                btnLoad.post {
+                    userAdapter.setData(users)
+                }
+            }.start()
         }
 
 
@@ -71,29 +86,45 @@ class HelloActivity1 : AppCompatActivity() {
         rvUsers.adapter = userAdapter
 
         //клики внутри списка
-        userAdapter.setListener(object: UserClickListener {
+        userAdapter.setListener(object : UserClickListener {
             override fun onItemClick(user: User) {
-                Toast.makeText(this@HelloActivity1, "onItemClick() user=$user", Toast.LENGTH_SHORT).show()
+                Thread {
+                    rvUsers.post {
+                        Toast.makeText(
+                            this@HelloActivity1,
+                            "onItemClick() user=$user",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }.start()
             }
 
             override fun onMenuDeleteClick(user: User) {
-                Toast.makeText(this@HelloActivity1, "onMenuDeleteClick() user=$user", Toast.LENGTH_SHORT).show()
+                Thread {
+                    db.deleteUserFromDB(user.login)
+                    val users = db.getAllUsers()
+                    val usersLog = users.joinToString(separator = ",\n")
+                    Log.d(TAG, "Users:\n $usersLog")
 
-                db.deleteUserFromDB(user.login)
-
-                val users = db.getAllUsers()
-                val usersLog = users.joinToString(separator = ",\n")
-                Log.d(TAG, "Users:\n $usersLog")
-                userAdapter.setData(users)
+                    rvUsers.post {
+                        userAdapter.setData(users)
+                        Toast.makeText(
+                            this@HelloActivity1,
+                            "onMenuDeleteClick() user=$user",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }.start()
             }
 
             override fun onMenuPassChangeClick(user: User) {
-                db.PassChangeClick(user)
-
+                Thread {
+                    rvUsers.post {
+                        db.PassChangeClick(user)
+                    }
+                }.start()
             }
-
         })
-
 
     }
 }
